corrected horizontal gradient

scripts/test_dyamond.py
@@ -140,18 +140,8 @@ if __name__ == '__main__':
     }
 
     for i, (level, prange) in enumerate(layers.items()):
+        # vertically integrated and time averaged fluxes
         data = dataset_fluxes.integrate_range(coord_range=prange).mean(dim='time')
-
-        pik = data.pi_hke
-        pia = data.pi_ape
-        pit = pik + pia
-
-        cad = data.cad
-        lct = data.lc
-        cdr = data.cdr
-
-        vfk = data.vfd_dke
-        vfa = data.vfd_ape
 
         # Create figure
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7.5, 5.8), constrained_layout=True)
@@ -162,18 +152,21 @@ if __name__ == '__main__':
         at.patch.set_boxstyle("round,pad=-0.3,rounding_size=0.2")
         ax.add_artist(at)
 
-        ax.semilogx(kappa, pit, label=r'$\Pi = \Pi_K + \Pi_A$',
+        ax.semilogx(kappa, data.pi_hke + data.pi_ape, label=r'$\Pi = \Pi_K + \Pi_A$',
                     linewidth=2.5, linestyle='-', color='k')
-        ax.semilogx(kappa, pik, label=r'$\Pi_K$', linewidth=1.6, linestyle='-', color='red')
-        ax.semilogx(kappa, pia, label=r'$\Pi_A$', linewidth=1.6, linestyle='-', color='navy')
 
-        ax.semilogx(kappa, cad, label=r'$C_{A\rightarrow D}$',
+        ax.semilogx(kappa, data.pi_hke, label=r'$\Pi_K$',
+                    linewidth=1.6, linestyle='-', color='red')
+        ax.semilogx(kappa, data.pi_ape, label=r'$\Pi_A$',
+                    linewidth=1.6, linestyle='-', color='navy')
+
+        ax.semilogx(kappa, data.cad, label=r'$C_{A\rightarrow D}$',
                     linewidth=1.6, linestyle='--', color='green')
-        ax.semilogx(kappa, cdr, label=r'$C_{D\rightarrow R}$',
+        ax.semilogx(kappa, data.cdr, label=r'$C_{D\rightarrow R}$',
                     linewidth=1.6, linestyle='-.', color='cyan')
 
-        # ax.semilogx(kappa, lct_l, label=r'$L_c$', linewidth=1.6, linestyle='--', color='orange')
-        ax.semilogx(kappa, vfk + vfa, label=r'$F_{\uparrow}(p_b) - F_{\uparrow}(p_t)$',
+        ax.semilogx(kappa, data.vfd_dke + data.vfd_ape,
+                    label=r'$F_{\uparrow}(p_b) - F_{\uparrow}(p_t)$',
                     linewidth=1.6, linestyle='-.', color='magenta')
 
         ax.set_ylabel(r'Cumulative energy flux ($W~m^{-2}$)', fontsize=15)
@@ -224,16 +217,8 @@ if __name__ == '__main__':
         xticks = np.array([2, 20, 200, 2000])
 
     for i, (level, prange) in enumerate(layers.items()):
-        # Integrate fluxes in layers
+        # vertically integrated and time averaged fluxes
         data = dataset_fluxes.integrate_range(coord_range=prange).mean(dim='time')
-
-        pid = data.pi_dke.values
-        pir = data.pi_rke.values
-
-        cdr_w = data.cdr_w.values
-        cdr_v = data.cdr_v.values
-        cdr_c = data.cdr_c.values
-        cdr = data.cdr.values - cdr_c
 
         # ------------------------------------------------------------------------------------------
         # Visualization of Kinetic energy budget
@@ -244,21 +229,21 @@ if __name__ == '__main__':
         at.patch.set_boxstyle("round,pad=-0.3,rounding_size=0.2")
         ax.add_artist(at)
 
-        # ax.semilogx(kappa, cad, label=r'$C_{A\rightarrow D}$',
-        #             linewidth=1.6, linestyle='-', color='green')
+        ax.semilogx(kappa, data.pi_dke + data.pi_rke, label=r'$\Pi_K$',
+                    linewidth=2., linestyle='-', color='k')
 
-        ax.semilogx(kappa, pid + pir, label=r'$\Pi_K$', linewidth=2., linestyle='-', color='k')
+        ax.semilogx(kappa, data.pi_dke, label=r'$\Pi_D$',
+                    linewidth=1.6, linestyle='-', color='green')
+        ax.semilogx(kappa, data.pi_rke, label=r'$\Pi_R$',
+                    linewidth=1.6, linestyle='-', color='red')
 
-        ax.semilogx(kappa, pid, label=r'$\Pi_D$', linewidth=1.6, linestyle='-', color='green')
-        ax.semilogx(kappa, pir, label=r'$\Pi_R$', linewidth=1.6, linestyle='-', color='red')
-
-        ax.semilogx(kappa, cdr, label=r'$C_{D \rightarrow R}$',
+        ax.semilogx(kappa, data.cdr - data.cdr_c, label=r'$C_{D \rightarrow R}$',
                     linewidth=2., linestyle='-', color='blue')
 
-        ax.semilogx(kappa, cdr_w, label=r'Vertical motion', linewidth=1.6,
+        ax.semilogx(kappa, data.cdr_w, label=r'Vertical motion', linewidth=1.6,
                     linestyle='-.', color='black')
 
-        ax.semilogx(kappa, cdr_v, label=r'Relative vorticity', linewidth=1.6,
+        ax.semilogx(kappa, data.cdr_v, label=r'Relative vorticity', linewidth=1.6,
                     linestyle='-.', color='red')
 
         # ax.semilogx(kappa, cdr_cl, label=r'Coriolis', linewidth=1.6,

src/io_tools.py
@@ -229,7 +229,7 @@ class SebaDataset(Dataset):
         return ds
 
     def get_field(self, name):
-        """ Returns a field in dataset after preprocessing:
+        """ Returns a field in dataset after preprocessing to be used by seba.EnergyBudget:
             - Exclude extrapolated data below the surface (p >= ps).
             - Ensure latitude axis is oriented north-to-south.
             - Reverse vertical axis from the surface to the model top.
@@ -249,6 +249,10 @@ class SebaDataset(Dataset):
 
         # Mask data pierced by the topography, if not already masked during interpolation.
         data = self[name].where(mask, np.nan)
+
+        # sort data north-south and starting at the surface
+        data = data.sortby([dim for dim in ["latitude", "level"]
+                            if dim in data.coords], ascending=False)
 
         info_coords = "".join(data.coords[name].axis.lower() for name in data.dims)
         data = data.to_masked_array()
@@ -681,7 +685,7 @@ def parse_dataset(dataset, variables=None, surface_data=None, p_levels=None):
 
             arrays[name] = surface_var
 
-    # Create output dataset with required fields.
+    # Create output dataset with required fields and check units consistency.
     data = SebaDataset(arrays).check_convert_units()
 
     # Check for pressure velocity: If not present in dataset, it is estimated from
@@ -692,6 +696,7 @@ def parse_dataset(dataset, variables=None, surface_data=None, p_levels=None):
         data['omega'] = omega
     else:
         # Searching for vertical velocity. Raise error if 'w_wind' cannot be found!
+        # This is neither 'omega' nor 'w_wind' are present in dataset.
         w_wind = dataset.find_variable('w_wind', raise_notfound=True)
 
         # Checking 'w_wind' units and converting to standard before computing omega.
@@ -703,16 +708,21 @@ def parse_dataset(dataset, variables=None, surface_data=None, p_levels=None):
         # add standard units [Ps/s]
         data.omega.attrs["units"] = expected_units['omega']
 
-    # Perform interpolation to constant pressure levels as needed (after all dynamic fields added)
-    # Ensures latitude dimension is ordered north-to-south and starting from the surface.
-    data = data.interpolate_levels(p_levels).sortby(["level", 'latitude'], ascending=False)
-
-    # Compute geopotential if not present in dataset
+    # Check if geopotential is present in dataset and add to data before interpolation
     phi = dataset.find_variable('geopotential', raise_notfound=False)
 
     if phi is not None:
         data['geopotential'] = phi
-    else:
+
+    # Perform interpolation to constant pressure levels if necessary (after all dynamic fields
+    # added). Ensures latitude dimension is ordered north-to-south and vertical levels starting
+    # at the surface.
+    data = data.interpolate_levels(p_levels).sortby(["level", 'latitude'], ascending=False)
+
+    # Computing geopotential if not found in dataset. This step must be done with data in
+    # pressure coordinates sorted from the surface up.
+    if 'geopotential' not in data:
+
         print("Computing geopotential from temperature ...")
 
         # reshape temperature to pass it to fortran subroutine
@@ -750,7 +760,8 @@ def parse_dataset(dataset, variables=None, surface_data=None, p_levels=None):
             # noinspection PyTypeChecker
             sfct = np.transpose(data.ts.values, axes=coord_order).reshape(data.dims["time"], -1)
 
-        # get surface elevation for the given grid.
+        # get surface elevation for the given grid. Using interpolated data
+        # from global dataset if it does not exist already.
         sfch = get_surface_elevation(data.latitude, data.longitude).values.ravel()
 
         # Compute geopotential from the temperature field: d(phi)/d(log p) = - Rd * T(p)

src/seba.py
@@ -308,6 +308,17 @@ class EnergyBudget:
                                           long_name='available potential energy per unit mass')
         return potential_energy
 
+    def energy_diagnostics(self):
+        """
+        Computes spectral energy budget and return as dataset objects.
+        """
+        # Compute diagnostics
+        rke, dke, hke = self.horizontal_kinetic_energy()
+        vke = self.vertical_kinetic_energy()
+        ape = self.available_potential_energy()
+
+        return SebaDataset(xr.merge([rke, dke, hke, vke, ape], compat="no_conflicts"))
+
     def nonlinear_energy_fluxes(self):
         """
         Computes each term in spectral energy budget and return as xr.DataArray objects.
@@ -324,7 +335,7 @@ class EnergyBudget:
         cdr_c = self.conversion_dke_rke_coriolis()
 
         # Total conversion from divergent to rotational kinetic energy.
-        c_dr = cdr_w + cdr_v + cdr_c
+        cdr = cdr_w + cdr_v + cdr_c
 
         # Compute cumulative nonlinear spectral energy fluxes
         pi_rke = self.rke_nonlinear_transfer()
@@ -362,7 +373,7 @@ class EnergyBudget:
                                          long_name='conversion from divergent to rotational '
                                                    'kinetic energy due to the coriolis effect')
 
-        fluxes['cdr'] = self.add_field(c_dr, units=units, standard_name='conversion_dke_rke',
+        fluxes['cdr'] = self.add_field(cdr, units=units, standard_name='conversion_dke_rke',
                                        long_name='conversion from divergent to '
                                                  'rotational kinetic energy')
 
@@ -773,17 +784,6 @@ class EnergyBudget:
 
         return dlog_gamma.reshape(-1) * heat_trans
 
-    def energy_diagnostics(self):
-        """
-        Computes spectral energy budget and return as dataset objects.
-        """
-        # Compute diagnostics
-        rke, dke, hke = self.horizontal_kinetic_energy()
-        vke = self.vertical_kinetic_energy()
-        ape = self.available_potential_energy()
-
-        return SebaDataset(xr.merge([rke, dke, hke, vke, ape], compat="no_conflicts"))
-
     # --------------------------------------------------------------------
     # Low-level methods for spectral transformations
     # --------------------------------------------------------------------
@@ -827,20 +827,57 @@ class EnergyBudget:
         Returns:
             Arrays containing gridded zonal and meridional components of the gradient vector.
         """
-        return self.sphere.getgrad(scalar)
+
+        # Compute horizontal gradient on grid-point space
+        scalar_gradient = self.sphere.getgrad(scalar)
+
+        if np.ma.is_masked(scalar):
+            # Let's apply a correction trick to remove boundary effects. Here, β is the
+            # terrain mask containing 0 for levels satisfying p > ps and 1 otherwise.
+            beta = (~scalar.mask).astype(float)
+
+            # Let's get the masked gradient using: β ∇φ = ∇(β φ) - (β φ) ∇β. The exact form of
+            # the last term should be φ∇β instead of (β φ) ∇β, however the two formulations are
+            # equivalent in non-masked regions (β = 1). We don't know φ anyway, only (β φ).
+            scalar_gradient -= self.sphere.getgrad(beta) * scalar
+
+        return scalar_gradient
 
     def _scalar_advection(self, scalar):
-        """
-        Compute the horizontal advection as dot product between
-        the wind vector and scalar gradient.
+        r"""
+        Compute the horizontal advection of a scalar field on the sphere.
 
-        scalar: scalar field to be advected
+        .. math:: \mathbf{u}\cdot\nabla_h\phi = \nabla_h\cdot(\phi\mathbf{u}) - \delta\phi
+
+        where :math:`\phi` is an arbitrary scalar, :math:`\mathbf{u}=(u, v)` is the horizontal
+        wind vector, and :math:`\delta` is the horizontal wind divergence.
+
+        Parameters:
+        -----------
+            scalar: `np.ndarray`
+                scalar field to be advected
+        Returns:
+        --------
+            advection: `np.ndarray`
+                Array containing the advection of a scalar field.
         """
+
         # computes the components of the scalar advection: (2, nlat, nlon, ...)
-        scalar_advection = self.wind * self.horizontal_gradient(scalar)
+        # scalar_advection = self.wind * self.horizontal_gradient(scalar)
+        # adding up horizontal components
+        # scalar_advection = np.ma.sum(scalar_advection, axis=0)
 
-        # returns the summed horizontal components
-        return np.ma.sum(scalar_advection, axis=0)
+        # Implementation of advection in flux form for a proper handling masked data.
+        # Spectral coefficients of the scalar flux divergence: ∇⋅(φu)
+        scalar_divergence = self._compute_rotdiv(scalar * self.wind)[1]
+
+        # back to grid-point space
+        scalar_divergence = self._inverse_transform(scalar_divergence)
+
+        # back to actual advection: u⋅∇φ = ∇⋅(φu) - δφ
+        scalar_advection = scalar_divergence - self.div * scalar
+
+        return scalar_advection
 
     def _wind_advection(self):
         r"""
@@ -913,15 +950,21 @@ class EnergyBudget:
 
     def vertical_gradient(self, scalar, vertical_axis=-1):
         """
-            Computes vertical gradient of a scalar function in pressure coordinates: d(scalar)/dp
+            Computes vertical gradient (∂φ/∂p) of a scalar function (φ) in pressure coordinates.
         """
-        scalar_grad = gradient_1d(scalar, self.pressure, axis=vertical_axis, order=8)
+        scalar_gradient = gradient_1d(scalar, self.pressure, axis=vertical_axis, order=8)
 
         # preserve the mask of scalar if masked
         if np.ma.is_masked(scalar):
-            scalar_grad = np.ma.masked_array(scalar_grad, mask=scalar.mask, fill_value=0.0)
+            # Let's apply a correction trick to remove boundary effects. Here, β is the
+            # terrain mask containing 0 for levels satisfying p >= ps and 1 otherwise.
+            beta = (~scalar.mask).astype(float)
+            beta_grad = gradient_1d(beta, self.pressure, axis=vertical_axis, order=8)
 
-        return scalar_grad
+            # Let's get the masked gradient using: β ∂φ/∂p = ∂(β φ)/∂p - (β φ) ∂β/∂p.
+            scalar_gradient -= beta_grad * scalar
+
+        return scalar_gradient
 
     def global_mean(self, scalar, weights=None, lat_axis=None):
         """
@@ -958,7 +1001,7 @@ class EnergyBudget:
                                  "Expected length {} but got {}.".format(self.nlat, weights.size))
 
         # Compute area-weighted average on the sphere (using either gaussian or linear weights)
-        # Added masked-arrays support to exclude data below the surface. "np.average" doesn't work!
+        # Added masked-arrays support to exclude data below the surface.
         scalar_average = np.ma.average(scalar, weights=weights, axis=lat_axis)
 
         # mean along the longitude dimension (same as lat_axis after array reduction)
@@ -1078,7 +1121,7 @@ class EnergyBudget:
     # Functions for preprocessing data:
     def _pack_levels(self, data, order='C'):
         # pack dimensions of arrays (nlat, nlon, ...) to (nlat, nlon, samples)
-        data_length = np.shape(data)[0]
+        data_length = len(data)
 
         if data_length == 2:
             new_shape = np.shape(data)[:3]

src/spherical_harmonics.py
@@ -95,7 +95,8 @@ class Spharmt(object):
         """Wrapper function for running _shtns functions along sample dimension"""
 
         # Compact args to a single array (input arrays must be broadcastable)
-        data = np.asarray(args)
+        # If passing masked arrays fill with zeros
+        data = np.ma.asarray(args).filled(fill_value=0.0)
 
         # check data type (real objects must be of type float64)
         if np.isrealobj(data):

src/tools.py
@@ -212,10 +212,9 @@ def transform_io(func, order='C'):
         transformed_args = [self, ]
         for arg in args:
             if isinstance(arg, np.ndarray):
-                # fill masked arrays before spectral transforms
-                arg = np.ma.fix_invalid(arg).filled(fill_value=0.0)
+                # fill masked arrays before spectral transforms (moved to 'spherical_harmonics')
+                # arg = np.ma.fix_invalid(arg).filled(fill_value=0.0)
                 transformed_args.append(self._pack_levels(arg, order=order))
-
         results = func(*transformed_args, **kwargs)
         # convert output back to original shape
         return self._unpack_levels(results, order=order)
@@ -671,9 +670,7 @@ def gradient_1d(scalar, x, axis=-1, order=6):
     # determine if the grid is regular
     dx = np.diff(x)
 
-    is_regular = np.allclose(np.max(dx), np.min(dx), atol=1e-12)
-
-    if is_regular:
+    if np.allclose(np.max(dx), np.min(dx), atol=1e-12):
         # Using high order schemes for regular grid, otherwise
         # using second-order accurate central finite differences
         scalar = np.moveaxis(scalar, axis, -1)