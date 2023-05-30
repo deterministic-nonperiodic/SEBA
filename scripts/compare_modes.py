fixed masked gradients

scripts/compare_modes.py
@@ -7,15 +7,15 @@ from seba import EnergyBudget
 warnings.filterwarnings('ignore')
 
 if __name__ == '__main__':
-    mode = "RO"
-    model = 'ICON'
+    mode = "IG"
+    model = 'IFS'
     resolution = 'n256'
     data_path = '/media/yanm/Data/DYAMOND/data/'
 
     date_time = '20200202'
     file_names = data_path + f"{model}_{mode}_inst_{date_time}_{resolution}.nc"
 
-    p_levels = np.linspace(1000e2, 10e2, 25)
+    p_levels = np.linspace(1000e2, 10e2, 41)
 
     # Create energy budget object
     budget = EnergyBudget(file_names, p_levels=p_levels, truncation=420)
@@ -40,8 +40,8 @@ if __name__ == '__main__':
     # Perform vertical integration along last axis
     layers = {'Free troposphere': [250e2, 450e2], 'Stratosphere': [20e2, 250e2]}
 
-    y_limits = {'Stratosphere': [-0.6, 1.0],
-                'Free troposphere': [-0.6, 1.0],
+    y_limits = {'Stratosphere': [-0.4, 0.8],
+                'Free troposphere': [-0.4, 0.8],
                 'Lower troposphere': [-1.0, 1.5]}
 
     figure_name = f'../figures/papers/{model}_{mode}_energy_fluxes_{resolution}.pdf'
@@ -57,13 +57,17 @@ if __name__ == '__main__':
     figure_name = f'../figures/papers/{model}_{mode}_hke_fluxes_{resolution}.pdf'
 
     layers = {'Free troposphere': [250e2, 450e2], 'Lower troposphere': [500e2, 850e2]}
-    y_limits = {'Free troposphere': [-0.04, 0.04], 'Lower troposphere': [-0.08, 0.08]}
+
+    y_limits = {
+        'IG': {'Free troposphere': [-0.04, 0.04], 'Lower troposphere': [-0.08, 0.08]},
+        'RO': {'Free troposphere': [-0.3, 0.3], 'Lower troposphere': [-0.3, 0.3]}
+    }
 
     # perform vertical integration
     dataset_fluxes.visualize_fluxes(model=model,
                                     variables=['pi_dke+pi_rke', 'pi_rke', 'pi_dke',
                                                'cdr', 'cdr_w', 'cdr_v'],
-                                    layers=layers, y_limits=y_limits, fig_name=figure_name)
+                                    layers=layers, y_limits=y_limits[mode], fig_name=figure_name)
 
     # ---------------------------------------------------------------------------------------
     # Visualize fluxes cross section

scripts/compare_spectrum.py
@@ -19,9 +19,8 @@ if __name__ == '__main__':
 
     # Load dyamond dataset
     model = 'ICON'
-    resolution = 'n1024'
-    data_path = '../data/'
-    # data_path = '/mnt/levante/energy_budget/test_data/'
+    resolution = 'n512'
+    data_path = '/media/yanm/Data/DYAMOND/simulations/'
 
     date_time = '20[0]'
     file_names = data_path + f"{model}_atm_3d_inst_{resolution}_gps_{date_time}.nc"
@@ -40,12 +39,14 @@ if __name__ == '__main__':
     f_sky = budget.representative_mean(beta)
 
     # visualize profiles
-    variables = ['omega', 'wind', 'theta_prime', 'theta']
+    variables = ['omega', 'theta_prime', 'wind_rot', 'wind_div']
     vars_info = {
-        'omega': ('scalar', r'Pressure velocity $(Pa^{2}~s^{-2})$'),
-        'theta': ('scalar', r'${\theta}^{2}~(K^{2})$'),
-        'theta_prime': ('scalar', r'${\theta^{\prime}}^{2}~(K^{2})$'),
-        'wind': ('vector', r'Horizontal kinetic energy  $(m^{2}~s^{-2})$')
+        'omega': ('scalar', r'Pressure vertical velocity $/~Pa^{2}~s^{-2}$'),
+        'phi': ('scalar', r'${\phi}^{2}~(K^{2})$'),
+        'theta_prime': ('scalar', r'${\theta^{\prime}}^{2}~/~K^{2}$'),
+        'wind': ('vector', r'Horizontal kinetic energy  $/~m^{2}~s^{-2}$'),
+        'wind_div': ('vector', r'Divergent kinetic energy  $/~m^{2}~s^{-2}$'),
+        'wind_rot': ('vector', r'Rotational kinetic energy  $/~m^{2}~s^{-2}$'),
     }
     pressure = 1e-2 * budget.pressure
 
@@ -54,6 +55,7 @@ if __name__ == '__main__':
                              constrained_layout=True)
 
     results = {}
+    lines = []
     # Compute spectrum of scalar variables
     for i, variable in enumerate(variables):
         ax = axes[i]
@@ -62,28 +64,36 @@ if __name__ == '__main__':
         if vars_info[variable][0] == 'vector':
             # The global average of the dot product of two vectors must equal the sum
             # of the vectors' cross-spectrum along all spherical harmonic degrees.
-            data_sqd = np.sum(data ** 2, axis=0)
-            data_gs = budget.representative_mean(data_sqd).mean(0)
-            data_sp = budget.cumulative_spectrum(budget._vector_spectrum(data))
+            data_gs = budget.representative_mean(np.ma.sum(data ** 2, axis=0)).mean(0)
+            data_clm = budget._vector_spectrum(data)
         else:
-            data_sqd = data ** 2
-            data_gs = budget.representative_mean(data_sqd).mean(0)
-            data_sp = budget.cumulative_spectrum(budget._scalar_spectrum(data))
+            data_gs = budget.representative_mean(data ** 2).mean(0)
+            data_clm = budget._scalar_spectrum(data)
+
+        masked = np.ma.is_masked(data)
+
+        data_sp = budget.cumulative_spectrum(data_clm, mask_correction=masked)
+        data_nc = budget.cumulative_spectrum(data_clm, mask_correction=False)
 
         # sum over all spherical harmonic degrees
         data_sp = np.nansum(data_sp, axis=0).mean(0)
+        data_nc = np.nansum(data_nc, axis=0).mean(0)
 
         # plot vertical profiles of reconstructed mean
         lines = ax.plot(data_gs.T, pressure, '-b',
-                        data_sp.T, pressure, '-.k', lw=2.5)
-        labels = [
-            'global mean',
-            'recovered',
-        ]
-        ax.legend(lines, labels, title=vars_info[variable][1], loc='best')
-        ax.set_ylim(1020, 20)
+                        data_sp.T, pressure, '--k',
+                        data_nc.T, pressure, '-.g', lw=2.5)
 
-    axes[0].set_ylabel('Pressure (hPa)')
+        ax.set_ylim(1020, 50)
+        ax.set_xlabel(vars_info[variable][1])
+
+    labels = [
+        'physical space',
+        'corrected spectrum',
+        'uncorrected spectrum'
+    ]
+    axes[0].legend(lines, labels, title='Integrated global variance', loc='best')
+    axes[0].set_ylabel('Pressure / hPa')
 
     plt.show()
     plt.close(fig)

scripts/wave_vortex_interaction.py
@@ -1,6 +1,7 @@
 import numpy as np
 import xarray as xr
 
+from io_tools import SebaDataset
 from seba import EnergyBudget
 from seba import parse_dataset
 
@@ -11,7 +12,7 @@ if __name__ == '__main__':
 
     date_time = '2020020[2]'
 
-    p_levels = np.linspace(1000e2, 10e2, 41)
+    p_levels = np.linspace(1000e2, 10e2, 31)
 
     dataset = {}
     dataset_fluxes = {}
@@ -23,7 +24,6 @@ if __name__ == '__main__':
 
         # compute cumulative energy fluxes
         dataset_fluxes[mode] = EnergyBudget(dataset[mode]).cumulative_energy_fluxes()
-        dataset_fluxes[mode] = dataset_fluxes[mode].cumulative_sum(dim='kappa')
 
     # Create dataset with full winds
     add_var = ['u_wind', 'v_wind', 'omega']
@@ -36,22 +36,24 @@ if __name__ == '__main__':
     budget = EnergyBudget(dataset)
 
     # compute cumulative energy fluxes
-    dataset_fluxes['FF'] = budget.cumulative_energy_fluxes().cumulative_sum(dim='kappa')
+    dataset_fluxes['FF'] = budget.cumulative_energy_fluxes()
 
-    wave_mean_fluxes = dataset_fluxes['FF'] - dataset_fluxes['IG'] - dataset_fluxes['RO']
+    wave_mean_fluxes = SebaDataset(
+        dataset_fluxes['FF'] - dataset_fluxes['IG'] - dataset_fluxes['RO']
+    )
 
     # ---------------------------------------------------------------------------------------
     # Visualize fluxes cross section
     # ---------------------------------------------------------------------------------------
     variables = ['cdr', 'vfd_dke', 'pi_hke']
 
-    fig_name = f'../figures/{model}_total_fluxes_section_n256.pdf'
-    dataset_fluxes['FF'].visualize_fluxes(model=None, variables=variables, y_limits=[1000., 10.],
-                                          fig_name=fig_name)
+    fig_name = f'../figures/papers/{model}_total_fluxes_section_n256.pdf'
+    dataset_fluxes['FF'].visualize_slices(model=None, variables=variables,
+                                          y_limits=[1000., 10.], fig_name=fig_name)
 
-    fig_name = f'../figures/{model}_wave_vortex_fluxes_section_n256.pdf'
-    wave_mean_fluxes.visualize_fluxes(model=None, variables=variables, y_limits=[1000., 10.],
-                                      fig_name=fig_name)
+    fig_name = f'../figures/papers/{model}_wave_vortex_fluxes_section_n256.pdf'
+    wave_mean_fluxes.visualize_slices(model=None, variables=variables,
+                                      y_limits=[1000., 10.], fig_name=fig_name)
 
     # ----------------------------------------------------------------------------------------------
     # Nonlinear transfer of Kinetic energy and Available potential energy
@@ -60,15 +62,15 @@ if __name__ == '__main__':
     # Perform vertical integration along last axis
     layers = {'Stratosphere': [20e2, 250e2], 'Free troposphere': [250e2, 450e2]}
 
-    y_limits = {'Stratosphere': [-0.8, 0.8],
-                'Free troposphere': [-0.5, 1.0],
+    y_limits = {'Stratosphere': [-0.8, 1.2],
+                'Free troposphere': [-0.5, 1.5],
                 'Lower troposphere': [-1.0, 1.5]
                 }
 
-    figure_name = f'../figures/papers/{model}_energy_fluxes_n256.pdf'
+    figure_name = f'../figures/papers/{model}_total_energy_fluxes_n256.pdf'
 
     dataset_fluxes['FF'].visualize_fluxes(model=model,
                                           variables=['pi_hke+pi_ape', 'pi_hke',
                                                      'pi_ape', 'cad', 'cdr', 'vfd_tot'],
                                           layers=layers, y_limits=y_limits,
-                                          resolution='n1024', fig_name=figure_name)
+                                          fig_name=figure_name)

seba/io_tools.py
@@ -303,7 +303,7 @@ class SebaDataset(Dataset):
 
                         if not same_size or not np.allclose(surface_var.latitude,
                                                             self.latitude, atol=1e-4):
-                            print(f'Interpolating surface data: {name}')
+                            print(f'Info: Interpolating surface data: {name}')
 
                             # extrapolate missing values usually points near the poles
                             surface_var = surface_var.interp(latitude=self.latitude,
@@ -386,8 +386,14 @@ class SebaDataset(Dataset):
         # masked elements are filled with zeros before the spectral analysis
         data.set_fill_value(0.0)
 
-        # prepare_data(data, info_coords)
-        return np.ma.transpose(data, axes=coord_order)
+        # transpose data to required shape
+        data = np.ma.transpose(data, axes=coord_order)
+
+        # get filled data if no mask is required
+        if not masked and np.ma.is_masked(data):
+            data = data.data
+
+        return data
 
     def _coordinate_range(self, name="level", limits=None):
         # Find vertical coordinate and sort 'coord_range' accordingly
@@ -880,7 +886,7 @@ def parse_dataset(dataset, variables=None, p_levels=None, **surface_data):
         sfcp = data.ps.values.ravel()
 
         if 'ts' not in data:
-            print("Computing surface temperature ...")
+            print("Info: Computing surface temperature ...")
 
             # compute surface temperature by linear interpolation
             sfct = numeric_tools.surface_temperature(sfcp, temperature, data.pressure.values)
@@ -893,12 +899,12 @@ def parse_dataset(dataset, variables=None, p_levels=None, **surface_data):
             # noinspection PyTypeChecker
             sfct = np.transpose(data.ts.values, axes=coord_order).reshape(data.dims["time"], -1)
 
-        print("Get surface elevation ...")
+        print("Info: Get surface elevation ...")
         # get surface elevation for the given grid. Using interpolated data
         # from global dataset if it does not exist already.
         sfch = get_surface_elevation(data.latitude, data.longitude).values.ravel()
 
-        print("Computing geopotential from temperature ...")
+        print("Info: Computing geopotential from temperature ...")
         # Compute geopotential from the temperature field: d(phi)/d(log p) = - Rd * T(p)
         phi = numeric_tools.geopotential(data.pressure.values, temperature, sfch, sfcp, sfct=sfct)
         phi = DataArray(phi.reshape(data_shape), dims=target_dims)
@@ -910,7 +916,7 @@ def parse_dataset(dataset, variables=None, p_levels=None, **surface_data):
     if data.latitude.size != data.longitude.size // 2:
         gridtype = 'gaussian'
 
-        print(f'Interpolating to {gridtype} grid ...')
+        print(f'Info: Interpolating to {gridtype} grid ...')
         latitude, _ = gaussian_lats_wts(data.longitude.size // 2)
 
         data = data.interp(latitude=latitude)
@@ -919,7 +925,7 @@ def parse_dataset(dataset, variables=None, p_levels=None, **surface_data):
 
     data.attrs.update(gridtype=gridtype)
 
-    print("Data processing completed successfully!")
+    print("Info: Data processing completed successfully!")
 
     # return dataset with proper variables and units (remove dask arrays)
     return data.compute()
@@ -994,7 +1000,7 @@ def interpolate_pressure_levels(dataset, p_levels=None):
         p_levels = np.array(p_levels)
         assert p_levels.ndim == 1, "'p_levels' must be a one-dimensional array."
 
-    print("Interpolating data to {} isobaric levels ...".format(p_levels.size))
+    print("Info: Interpolating data to {} isobaric levels ...".format(p_levels.size))
 
     dims = get_coordinate_names(dataset)
     coords = [dataset.coords[name] for name in dims]
@@ -1024,7 +1030,7 @@ def interpolate_pressure_levels(dataset, p_levels=None):
         else:
             excluded_vars.append(name)
 
-    print("Interpolation finished successfully.")
+    print("Info: Interpolation finished successfully.")
 
     # replace pressure field in dataset with the new coordinate
     attrs = {'standard_name': "pressure", 'units': "Pa"}

seba/seba.py
@@ -12,8 +12,7 @@ from spectral_analysis import kappa_from_deg
 from spherical_harmonics import Spharmt
 from thermodynamics import exner_function, potential_temperature
 from thermodynamics import lorenz_parameter, vertical_velocity
-from tools import prepare_data, transform_io
-from tools import rotate_vector, broadcast_1dto, gradient_1d
+from tools import prepare_data, transform_io, rotate_vector, broadcast_1dto, gradient_1d
 
 # declare global read-only variables
 _global_attrs = {'source': 'git@github.com:deterministic-nonperiodic/SEBA.git',
@@ -137,13 +136,14 @@ class EnergyBudget:
         self.pressure = data.pressure.values
 
         # get vertical velocity
-        self.omega = data.get_field('omega')
+        self.omega = data.get_field('omega', masked=True)
 
         # Get geopotential field and compute geopotential height
-        self.phi = data.get_field('geopotential')
+        self.phi = data.get_field('geopotential', masked=True)
 
         # create wind array from masked wind components (preserving mask)
-        self.wind = np.ma.stack((data.get_field('u_wind'), data.get_field('v_wind')))
+        self.wind = np.ma.stack((data.get_field('u_wind', masked=True),
+                                 data.get_field('v_wind', masked=True)))
 
         # compute thermodynamic quantities. Using unmasked temperature field if possible
         # Using masked temperature causes issues computing the spectral transfers of APE,
@@ -158,6 +158,9 @@ class EnergyBudget:
         # define data mask once for consistency
         self.mask = self.omega.mask
         self.beta = (~self.mask).astype(float)
+
+        # # unmask omega after retrieving the mask
+        # self.omega = np.ma.fix_invalid(self.omega, fill_value=0.0)
 
         # Compute fraction of valid points at every level for Spectral Mode-Coupling Correction
         # same as the power-spectrum of the mask integrated along spherical harmonic degree. This